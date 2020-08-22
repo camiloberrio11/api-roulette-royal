84dbbaedc2cf543048439bc1b5bf98f7901bf00d
jberrio <jberrio@transfiriendo.com>
2020-08-22 09:41:02 -0500

    Modificaciones de server sin morgan ni urlencoded

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict'
 const express = require('express')
 const path = require('path')
-const morgan = require('morgan')
+// const morgan = require('morgan')
 const routes = require('./routes/routes')
 const portSecure = process.env.PORT || '8094'
 const { startPageApi } = require('./controllers/startpage.controller')
@@ -9,7 +9,7 @@ const { startPageApi } = require('./controllers/startpage.controller')
 const app = express()
 // app.use(morgan('dev'))
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+// app.use(express.urlencoded({ extended: false }))
 app.set('public', path.join(__dirname, '/public'))
 app.get('/', startPageApi)
 app.use('/api', routes)

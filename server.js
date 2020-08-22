9e7c2c96eee891d5381df370a1f0d7f6b7be24cc
jberrio <jberrio@transfiriendo.com>
2020-08-22 09:38:24 -0500

    Modificaciones de server

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const { startPageApi } = require('./controllers/startpage.controller')
 
 const app = express()
 app.use(morgan('dev'))
-app.use(express.json())
+// app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.set('public', path.join(__dirname, '/public'))
 app.get('/', startPageApi)
-app.use('/api', routes)
+// app.use('/api', routes)
 app.listen(portSecure, () => {
   console.log(`Server running on port: ${portSecure}`)
 })

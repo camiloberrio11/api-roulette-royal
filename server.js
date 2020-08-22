097e369af2679ce47df46c296ce2c38ae593abe2
jberrio <jberrio@transfiriendo.com>
2020-08-22 09:39:26 -0500

    Modificaciones de server sin morgan

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const portSecure = process.env.PORT || '8094'
 const { startPageApi } = require('./controllers/startpage.controller')
 
 const app = express()
-app.use(morgan('dev'))
-// app.use(express.json())
+// app.use(morgan('dev'))
+app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.set('public', path.join(__dirname, '/public'))
 app.get('/', startPageApi)
-// app.use('/api', routes)
+app.use('/api', routes)
 app.listen(portSecure, () => {
   console.log(`Server running on port: ${portSecure}`)
 })

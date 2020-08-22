6941b25a35051361bd64c50dec29eb40f639f914
jberrio <jberrio@transfiriendo.com>
2020-08-22 18:19:51 -0500

    Se modifica script para que el numero acepte null

--- database/sql/betOnRoulette/betOnRoulette.scripts.js
+++ database/sql/betOnRoulette/betOnRoulette.scripts.js
@@ -3,7 +3,7 @@ const scriptCreateBet = (bet) => {
   const { idBet, isRed, isBlack, betNumber, money, idroulette, userid } = bet
   return `
   INSERT INTO bet_on_roulette(idbet, isred, isblack, number_bet, money, userid, idroulette)
-  VALUES (${+idBet}, ${Boolean(isRed)}, ${Boolean(isBlack)}, ${+betNumber}, ${+money}, '${userid}', ${+idroulette})
+  VALUES (${+idBet}, ${Boolean(isRed)}, ${Boolean(isBlack)}, ${betNumber}, ${+money}, '${userid}', ${+idroulette})
   `
 }
 module.exports = {
